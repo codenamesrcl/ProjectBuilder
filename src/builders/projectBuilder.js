fb10f38811c89bcae57db8a308e4222736e70d37
unknown <cplee@MAAPPDEV-107.ropesgray.firm>
2015-09-15 10:43:33 -0400

    updated the included projectBuilder builder config to reflect the changes made to the config creation process

--- src/builders/projectBuilder.js
+++ src/builders/projectBuilder.js
@@ -2,35 +2,16 @@
 //can also be refered to as the project working directory
 var project_root = ""; 
 
+var builderFactory = require(process.cwd() + '/builders/baseBuilder/builderFactory');
+
+var _buildPath = '/app/_builds/';
+
 var angularConfig = {
-    gen: {
-        srcRoot: project_root + "/app/gen/",
-        
-        cacheRoot: '/app/gen/',
-
-        appModuleName: "gen",
-
-        destPath: project_root + '/app/_builds/'
-    },
-    manager: {
-        srcRoot: project_root + "/app/manager/",
-        
-        cacheRoot: '/app/manager/',
-
-        appModuleName: "manager",
-
-        destPath: project_root + '/app/_builds/'
-    },
-    nodetools: {
-        srcRoot: project_root + "/app/nodetools/",
-        
-        cacheRoot: '/app/nodetools/',
-
-        appModuleName: "nodetools",
-
-        destPath: project_root + '/app/_builds/'
-    },
+    gen: builderFactory.create('gen', project_root, '/app/gen/', _buildPath),
+    manager: builderFactory.create('manager', project_root, '/app/manager/', _buildPath),
+    nodetools: builderFactory.create('nodetools', project_root, '/app/nodetools/', _buildPath)
 }
+
 
 module.exports = {
     config: {
